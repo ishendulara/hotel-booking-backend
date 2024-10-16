cfab0ce14ffe30ab411a80c64196394f572cbaa2
Ishen Dulara Kulasinghe <ishendulara9@gmail.com>
2024-10-17 00:32:45 +0530

    dekete function create

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -5,5 +5,7 @@ import { postCategory} from "../controllers/categoryController.js";
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", postCategory);
+categoryRouter.delete("/:name");
+
 
 export default categoryRouter;//fix
