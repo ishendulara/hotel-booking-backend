7e01cd2336df0167c5a5c4a12916af4e58b917a1
IshenDulara <ishendulara9@gmail.com>
2025-02-07 00:28:52 +0530

    goto route

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,6 +1,7 @@
 // routes/categoryRoute.js
 import express from "express";
-import { postCategory, deleteCategory, getCategory, getCategoryByName } from "../controllers/categoryController.js";
+import { postCategory, deleteCategory, getCategory, getCategoryByName, updateCategory } from "../controllers/categoryController.js";
+
 const categoryRouter = express.Router();
 
 categoryRouter.post("/", postCategory)
@@ -10,5 +11,7 @@ categoryRouter.delete("/:name",deleteCategory)
 categoryRouter.get("/",getCategory)
 
 categoryRouter.get("/:name",getCategoryByName)
+
+categoryRouter.update("/:name",updateCategory)
 
 export default categoryRouter;//fix
