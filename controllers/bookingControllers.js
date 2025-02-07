cdd80f36dd5e228f7835bebe0232de2367bc6c05
IshenDulara <ishendulara9@gmail.com>
2025-02-08 00:41:20 +0530

    booking creation function done

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -15,7 +15,38 @@ export function createBooking(req,res){
     Booking.countDocuments({}).then(
         (count)=>{
            console.log(count);
-           const newId = "INV"+startingId + count + 1;
+           const newId =startingId + count + 1;
+           const newBooking = new Booking({
+            bookingId : newId,
+            roomId : req.body.roomId,
+            email : req.body.email,
+            start : req.body.start,
+            end : req.body.end
+           })
+           newBooking.save().then(
+            (result)=>{
+                res.json(
+                    {
+                        message : "Booking created Succesfully",
+                        result : result
+                    }
+                )
+            }
+           ).catch(
+            (err)=>{
+                res.json({
+                    message : "Booking creation Failed",
+                    error : err
+                })
+            }
+           )
+        }
+    ).catch(
+        (err)=>{
+            res.json({
+                message : "Booking Creation Failed",
+                error : err
+            })
         }
     )
 }
