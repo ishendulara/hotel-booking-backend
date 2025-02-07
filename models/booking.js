399d961ecdfb8bc7fd3139f71524fb18f41b82e7
IshenDulara <ishendulara9@gmail.com>
2025-02-07 23:43:56 +0530

    booking model done

--- models/booking.js
+++ models/booking.js
@@ -7,13 +7,34 @@ const bokkingSchema = new mongoose.Schema(
             required : true,
             unique : true
         },
+        roomId:{
+            type : Number,
+            required : true
+        },
         email:{
             type : String,
             required : true,
         },
         status:{
             type : String,
-            
+            required : true,
+            default : "pending"
+        },
+        reason:{
+            type : String,
+            default : ""
+        },
+        start:{
+            type : Date,
+            required : true
+        },
+        end:{
+            type : Date,
+            required : true
+        },
+        notes:{
+            type : String,
+            default : ""
         }
     }
 )
