dd05f458f9737e19d607479b84e680ba817dfa1e
IshenDulara <ishendulara9@gmail.com>
2025-02-08 00:26:06 +0530

    change b1

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -1,13 +1,21 @@
 import Booking from "../models/booking.js";
+import { isCustomerValide } from "./userControllers.js";
 
 export function createBooking(req,res){
+
+    if(!isCustomerValide(req)){
+        res.status(403).json({
+            message : "Forbidden"
+        })
+        return
+    }
 
     const startingId = 1200;
 
     Booking.countDocuments({}).then(
         (count)=>{
            console.log(count);
-           const newId = "INV"+startingId + count;
+           const newId = "INV"+startingId + count + 1;
         }
     )
 }
