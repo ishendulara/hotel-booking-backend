a0aa635bdcfca583a199947c94b4cf0a004a9536
IshenDulara <ishendulara9@gmail.com>
2025-02-14 00:17:30 +0530

    done

--- routes/bookingRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createBooking } from '../controllers/bookingControllers.js';
-
-
-const bookingRouter = express.Router();
-
-bookingRouter.post("/",createBooking)
-
-export default bookingRouter; 
